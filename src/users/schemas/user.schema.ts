e449cc199bab7c433cd44b9341cb9060f3ace1a5
Nguyễn Hữu Thịnh <nguyenhthinh0203@gmail.com>
2026-03-05 13:32:43 +0700

    hotfix: logout auth

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -6,20 +6,20 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-    @Prop({ required: true, unique: true })
-    username: string;
+  @Prop({ required: true, unique: true })
+  username: string;
 
-    @Prop({ required: true, unique: true })
-    email: string;
+  @Prop({ required: true, unique: true })
+  email: string;
 
-    @Prop({ required: true })
-    passwordHash: string;
+  @Prop({ required: true })
+  passwordHash: string;
 
-    @Prop()
-    refreshTokenHash?: string;
+  @Prop()
+  refreshTokenHash?: string;
 
-    @Prop({ type: [String], enum: Role, default: [Role.MEMBER] })
-    roles: Role[];
+  @Prop({ type: [String], enum: Role, default: [Role.MEMBER] })
+  roles: Role[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
